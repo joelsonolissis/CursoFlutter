efe333a061b90bd0214551f22bd1b96292169136
joelsonolissis <olissismorais1@outlook.com>
2021-03-22 01:23:21 +0000

    aprendendo a gerenciar estados e darktheme

---
+++ aula6/lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'app_cobntroller.dart';
+
+main(){
+  runApp(AppWidget(
+    title: 'Flutterando 3'
+  ));
+}
+class AppWidget extends StatelessWidget{
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+  @override
+  Widget build(BuildContext context){
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (conext, child){
+      return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          brightness: AppController.instance.darkTheme ? Brightness.light : Brightness.dark ,
+        ),
+        home: HomePage(),
+      );
+    },
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget{
+
+@override
+State<StatefulWidget> createState(){
+ return HomePageState();
+}
+}
+
+class HomePageState extends State<HomePage>{
+  int counter = 0;
+
+Widget build(BuildContext context) {
+  return Scaffold(
+      appBar: AppBar(
+        title: Text('Joelson Morais'),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        setState(() {
+       counter++;
+     });
+      },
+      child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Switch(
+          value: AppController.instance.darkTheme, 
+          onChanged: (value){
+            AppController.instance.changeTheme();
+          }),
+        
+      ),
+    );
+}
+}
